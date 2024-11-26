b2f2f4ebd18f460d364da91fe402c3ee1f931189
Dominick Mpanduji <mpanduji.dominick@gmail.com>
2024-11-26 13:11:49 +0300

    feature homepage curves done

---
+++ src/app/components/FeaturesSection.tsx
@@ -0,0 +1,41 @@
+
+const FeaturesSection = () => {
+  return (
+    <div className="flex w-full h-[430px]  bg-white  justify-center items-center">
+         <div className="absolute z-30 left-0   w-[800px] h-[80px]  top-[763px] bg-[#2F333E]"
+            style={{
+                transform: 'skew(-35deg, 0deg)',
+                borderBottomRightRadius:'35px',
+                marginLeft:'-100px'
+            }}
+            >
+            </div>
+            <div className="absolute z-20 left-300   w-[1300px] h-[70px]  top-[750px] bg-white"
+            style={{
+                transform: 'skew(-35deg, 0deg)',
+                borderTopLeftRadius:'36px',
+                marginLeft:'806px'
+            }}
+            >
+            </div>
+        <div className="flex w-[1368px] h-[430px] bg-green-500">
+            <div className=" z-10 flex-col w-full bg-white">
+
+            </div>
+            <div className=" z-10 flex-col w-full bg-white ">
+
+            </div>
+            {/* curved div */}
+           
+
+        </div>
+
+
+
+    </div>
+  )
+}
+
+
+
+export default FeaturesSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import HeaderSection from "./components/HeaderSection";
+import FeaturesSection from "./components/FeaturesSection";
 
 export default function Home() {
   return (
+    <>
       <HeaderSection/>
-
-     
+      <FeaturesSection/>
+    </>
+      
   );
 }
