9151c1fb2f0f7f2060fb31157c1c4e2f1f2a3dfb
Dominick Mpanduji <mpanduji.dominick@gmail.com>
2024-11-26 12:02:22 +0300

    feature header well done

--- src/app/components/HeaderSection.tsx
+++ src/app/components/HeaderSection.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 
@@ -102,82 +103,98 @@ const ColumnLayoutEdgesRight = () => {
 }
 
 
+const LinkStyle = {
+    display:'block',
+    fontFamily:'Avenir-Light',
+    fontSize:'17px',
+    color:'white',
+    padding:'6px 0px 6px 0px',
+    marginLeft:'1em',
+    marginRight:'1em'
 
+}
 
-// const HeaderSection = () => {
-//   return (
-//     <>
-//     <div style={headerStyle} className="w-full h-[764px]  flex items-center justify-center">
-//         <div className=" z-10 flex-1 bg-red-500 h-[764px] flex items-center justify-center">
-//             <p className=" text-white text-lg">Div 1</p>
-//         </div>
-//       <div className="flex-1  h-[764px]  bg-[url('/assets/spacecraft.png')] bg-cover bg-center  flex items-center justify-between   " style={{backgroundSize: '80%'}}>
-//             {/* primary color background blending */}
-//             <div className="absolute  z-0 h-[764px] w-[100%]   inset-0 bg-[#2F333E] mix-blend-color" ></div>
+const ActiveLinkStyle = {
+    display:'block',
+    fontFamily:'Avenir-Light',
+    fontSize:'17px',
+    color:'#FFA200',
+    padding:'4px 0px 4px 0px',
+    borderTop: '2px solid #FFA200',
+    borderBottom: '2px solid #FFA200',
+    marginLeft:'1em',
+    marginRight:'1em'
+    
+}
+const AuthLinkStyle = {
+    display:'block',
+    fontFamily:'Avenir-Light',
+    fontSize:'17px',
+    color:'white',
+    padding:'0.5em 1em',
+    border: '1px solid white',
+    borderRadius: '5px',
+    marginLeft:'1em',
+    marginRight:'1em',
+   
+    
+}
 
-//             {/* <ThreeColumnLayout/> */}
+const Navlinks = ()=>{
+    return (
+        <>
+            <div className="flex ">
+                <div>
+                    <Link href="/Home"  style={ActiveLinkStyle}>
+                        Home
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/Features"  style={LinkStyle}>
+                        Features
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/Documentation"  style={LinkStyle}>
+                        Documentation
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/Demo"  style={LinkStyle}>
+                        Demo
+                    </Link>
+                </div>
+            </div>        
+        </>
+    )
+}
 
-//             <ColumnLayoutEdges/>
-//             <ColumnLayout/>
-//             <ColumnLayoutEdges/>
+const AuthLinks = ()=>{
+    return (
+        <>
+            <div className="flex ">
+                <div>
+                    <Link href="/Home"  style={LinkStyle}>
+                        Log In
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/Features"  style={AuthLinkStyle}>
+                        Register
+                    </Link>
+                </div>
+            </div>        
+        </>
+    )
+}
 
-
-//       </div>
-//     </div>
-
-//       <div className="flex  justify-center items-center z-20 w-full h-[764px]   " style={{position:'absolute',top:0}}>
-//             <div className="flex-1 bg-[#2F333E] h-[764px] flex-col  " style={{paddingTop:'10em',paddingLeft:'16em'}}>
-//                 <div className="flex">
-//                      <h1 style={{fontFamily:'Chillax-Bold',fontSize:"46px"}} className="text-white ">Experience the Power<br/> of <span style={{color:"#DC8F0A"}}>PAWA</span>, The Swahili<br/> Language AI Model</h1>
-//                 </div>
-//                 <div className="flex pt-8">
-//                     <p className="avenir" style={{fontSize:"17px",color:'white'}}>Unlock the potential of Swahili language understanding<br/> with our state-of-the-art AI model.</p>
-//                 </div>
-
-//                 <SearchDiv/> 
-//                 <div className="flex flex-row ">
-//                     <div className="flex  flex-row mt-20 mr-20"> 
-//                         <div>
-//                             <p className="text-white mr-4 text-[17px]">Try PAWA Now</p>
-//                         </div>
-//                         <div >
-//                             <div ><img height={27} width={27} src="/assets/arrow.png" alt="" /></div>
-//                         </div>
-//                     </div>
-//                     <div className="flex  flex-row mt-20"> 
-//                         <div>
-//                             <p className="text-white mr-4 text-[17px]">Get Started</p>
-//                         </div>
-//                         <div >
-//                             <div ><img height={27} width={27} src="/assets/arrow.png" alt="" /></div>
-//                         </div>
-//                     </div>
-//                 </div>
-
-//             </div>
-//             <div className="flex-1  h-[764px] flex items-center justify-center">
-//                 {/* right and left gradients */}
-//                      <div className="flex  h-[764px]  w-[50%]  " style={imageGrad1}></div>
-//                      <div className="flex  h-[764px]  w-[50%] " style={imageGrad2}></div>
-//                  {/* right and left gradients */}
-//             </div>
-
-
-
-
-//       </div>
-//         </>
-//   )
-// }
 
 
 const HeaderSection = () => {
     return (
         <>
             <div className="flex justify-center items-center h-[764px] bg-[#2F333E]  ">
-
-
-                <div style={headerStyle} className="w-[1366px]  h-[764px]  flex items-center justify-center">
+                <div style={headerStyle} className="w-[1366px] z-10  h-[764px]  flex items-center justify-center">
                     <div className=" z-10 flex w-full bg-red-500 h-[764px] flex items-center justify-center">
                         <p className=" text-white text-lg">Div 1</p>
                     </div>
@@ -221,8 +238,6 @@ const HeaderSection = () => {
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
                     <div className="flex w-full  h-[764px] flex items-center justify-center">
                         {/* right and left gradients */}
@@ -232,12 +247,18 @@ const HeaderSection = () => {
 
                     </div>
 
+                    <div className="flex items-center justify-between  absolute z-100  w-full h-[140px] top-0 ">
 
+                        <div>
+                            <img src="/assets/pawa-logo.png" height={"45px"} width={"45px"} alt="" />
+                        </div>
+                        <div><Navlinks/></div>
+                        <div><AuthLinks/></div>
+
+                    </div>
                 </div>
-
-
-
             </div>
+           
 
         </>
     )
